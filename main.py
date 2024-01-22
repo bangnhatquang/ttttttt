390bddbe3f968487f47d693f4f2eade010c8d2d5
bangnhatquang <156689126+bangnhatquang@users.noreply.github.com>
2024-01-22 18:05:04 +0700

    main.py

--- main.py
+++ main.py
@@ -1,44 +1,28 @@
-
-from kivy.app import App
 from kivy.lang import Builder
-from kivy.uix.boxlayout import BoxLayout
-import time
-Builder.load_string('''
-<CameraClick>:
-    orientation: 'vertical'
-    Camera:
-        id: camera
-        resolution: (640, 480)
-        play: False
-    ToggleButton:
-        text: 'Play'
-        on_press: camera.play = not camera.play
-        size_hint_y: None
-        height: '48dp'
-    Button:
-        text: 'Capture'
-        size_hint_y: None
-        height: '48dp'
-        on_press: root.capture()
-''')
+
+from kivymd.app import MDApp
+
+KV = '''
+MDScreen:
+
+    MDFloatingActionButtonSpeedDial:
+        data: app.data
+        root_button_anim: True
+'''
 
 
-class CameraClick(BoxLayout):
-    def capture(self):
-        '''
-        Function to capture the images and give them the names
-        according to their captured time and date.
-        '''
-        camera = self.ids['camera']
-        timestr = time.strftime("%Y%m%d_%H%M%S")
-        camera.export_to_png("IMG_{}.png".format(timestr))
-        print("Captured")
-
-
-class TestCamera(App):
+class Example(MDApp):
+    data = {
+        'Python': 'language-python',
+        'PHP': 'language-php',
+        'C++': 'language-cpp',
+    }
 
     def build(self):
-        return CameraClick()
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "Orange"
+        return Builder.load_string(KV)
 
 
-TestCamera().run()
+if __name__ == '__main__':
+    Example().run()
